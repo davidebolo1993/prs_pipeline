b5a13def310bd6cf225b0ab0125b9c2ad60c0d15
davidebolo1993 <davidebolognini7@gmail.com>
2022-09-28 14:35:53 +0200

    adding convenient way to run bash + sbatch in a single command

--- scripts/sum_prs.sh
+++ scripts/sum_prs.sh
@@ -3,7 +3,24 @@
 eval "$(conda shell.bash hook)"
 conda activate bigsnprenv_1911
 outdir="/path/to/outdir"
+
 #assume the output folder has the same tree structure from bgen_parallel.sbatch
 
+#run bgen_parallel
+sentence=$(sbatch bgen_parallel.sbatch)
+
+#wait until all the job ids are consumed
+stringarray=($sentence)
+jobid=(${stringarray[3]})
+VAR=0
+while [ "$VAR" -eq 0 ]; do
+    sleep 1
+    nrow=$(squeue -j $jobid | wc -l)
+    if [ $nrow -eq 1 ] ; then #only the header of the jobid table
+        VAR=1
+    fi
+done
+
+#sum
 head -1 $outdir/*/*.prs.tsv | grep -v "^=" | tail -1 | cut -f 1-3 > $outdir/all.prs.tsv
 tail -n+2 $outdir/*/*.prs.tsv | grep -v "^=" | sort -V | sed '1{/^ *$/d}' | datamash groupby 1,2 sum 3 >> $outdir/all.prs.tsv
